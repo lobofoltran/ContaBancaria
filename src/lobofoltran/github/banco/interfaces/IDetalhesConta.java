33e6be1fcd63d55a57cb8639042b3271a1b1a99f
Gustavo Lobo <gustavolobo@alunos.unicesumar.edu.br>
2022-03-16 22:07:45 -0300

    Delete src/lobofoltran/github directory

--- src/lobofoltran/github/banco/interfaces/IDetalhesConta.java
+++
@@ -1,7 +0,0 @@
-package lobofoltran.github.banco.interfaces;
-
-public interface IDetalhesConta {
-
-	public void detalhes();
-	
-}
--- src/lobofoltran/github/banco/interfaces/IGeraExtratoEmXml.java
+++
@@ -1,9 +0,0 @@
-package lobofoltran.github.banco.interfaces;
-
-public interface IGeraExtratoEmXml {
-
-	public void salvaArquivoXml();
-	public void constroiArquivoXml();
-	public void validaArquivoXml();
-	
-}
